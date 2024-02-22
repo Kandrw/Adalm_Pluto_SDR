47fda4bd6c1169c16206b6cc130c430500f61d28
Kandrw <98390518+Kandrw@users.noreply.github.com>
2024-02-22 14:40:22 +0700

    add generate QAM

--- src/sample/sample.py
+++ src/sample/sample.py
@@ -3,16 +3,9 @@ import numpy as np
 
 #Строку символов в бинарный вид
 def data_to_byte(data):
-    #print("[data_to_byte]")
     bin_str = ''.join(format(ord(i), '08b') for i in data)
     return np.array(list(map(int, list(bin_str))))
-
-    
-
-
 def encode_QPSK(data, mode):
-    
-    
     ampl = 2**1
     if (len(data) % 2 != 0):
         print("QPSK:\nError, check data length", len(data))
@@ -29,7 +22,6 @@ def encode_QPSK(data, mode):
         sample = np.asarray(sample)
         sample = sample * ampl
         return sample
-
 def duplication_sample(data, N):
     dup_buff = []
     
@@ -38,6 +30,36 @@ def duplication_sample(data, N):
             dup_buff.append(i)
     return dup_buff
 
+def formula(value, k):
+    return (k - 2 * value)    
+def recurse_formula(arr_values, n, pos):
+    if(len(arr_values) < 2):
+        return 1
+    return ( 2**n - (1 - 2 * arr_values[pos]) * recurse_formula(arr_values[2:], n-1, pos))
 
 
+def calc_coeff(n):
+    if(n < 1):
+        return 2
+    return 2 + 4 * calc_coeff(n-1)
+
+def encode_QAM(data_bit, N):#[0, 1, 0, 1, ....], уровень QAM
+    ampl = 2**14
+    ad = int(np.log2(N))
+    if (len(data_bit) % 2 != 0):
+        print("QPSK:\nError, check bit_mass length", len(data_bit))
+        raise "error"
+        return
+    sample = [] # массив комплексных чисел
+    k1 = calc_coeff(int(np.log2(N))/2)
+    for i in range(0, len(data_bit), int(np.log2(N))):
+        sr = data_bit[i:i+int(np.log2(N))]
+        sr = list(reversed(sr))
+        d1 = formula(sr[0], 1)
+        d2 = recurse_formula(sr[2:], int(np.log2(N))/2, 0)
+        d1i = formula(sr[1], 1)
+        d2i = recurse_formula(sr[2:], int(np.log2(N))/2, 1)
+        dd = (d1 * d2) /np.sqrt(k1) + ((d1i * d2i) / np.sqrt(k1)) * 1j
+        sample.append(dd)
+    return sample
 
--- tests/test_signal.py
+++ tests/test_signal.py
@@ -15,36 +15,37 @@ import tx_rx.conf as conf
 
 import sample.sample as sample
 
-
+PLATFORM = "Win"
 
 rf_module = conf.RxTx()
-
-
 rf_module.print_parameters()
 
 
-dir_gen_packet = "../src/generate_packet/"
+dir_gen_packet = "..\src\generate_packet\\"
 argvs = ['data.txt', 'gold_sequence.txt', 'gold_seq_end.txt', 'data_bin.txt' ]
 
 for i in range(len(argvs)):
     argvs[i] = dir_gen_packet + argvs[i]
 
 module_gen_header = "generate_packet"
+if(PLATFORM == "Win"):
+    module_gen_header += ".exe"
 
-data = "Test"
+    
+data = "Test data"
 
 file = open(argvs[0], "w")
 file.write(data)
 file.close()
 #sys.exit()
 subprocess.call([dir_gen_packet + module_gen_header] + argvs)
+
 time.sleep(1)
 
 file = open(argvs[-1], "r")
 
 data_bin = file.read()
 data_bin = list(map(int, data_bin))
-#data_bin = data_bin[:10]
 for i in data_bin:
     print(i, end="")
 
@@ -52,9 +53,14 @@ plt.figure(1, figsize=(10, 10))
 plt.subplot(2, 2, 1)
 plt.title("Data")
 plt.plot(data_bin)
+
+
+
 N = 10
 data_rep = sample.duplication_sample(data_bin, N)
-data_qpsk = sample.encode_QPSK(data_rep, 4)
+#data_qpsk = sample.encode_QPSK(data_rep, 4)
+data_rep = data_bin
+data_qpsk = np.array(sample.encode_QAM(data_rep, 4))
 
 plt.subplot(2, 2, 2)
 plt.title("Data duplicate")
@@ -68,10 +74,10 @@ plt.scatter(data_qpsk.real, data_qpsk.imag)
 h1 = np.ones(N)
 # Noise
 pos_read = 0
-noise_coef = 0.01
+noise_coef = 0.0001
 data_noise = data_qpsk[0:] 
 noise = np.random.normal(0, noise_coef, len(data_noise))
-data_noise += noise
+#data_noise += noise
 
 plt.subplot(2, 2, 4)
 plt.title(f"BPSK + noise({noise_coef})")
@@ -104,41 +110,14 @@ def gardner_TED(data):
         errors[i] = (data.real[i-1]) % N
     
 
-def rrc_filter(N, beta):
-# Impulse response of the root rise cosine filter given the number of samples
-# Inputs:
-# beta: roll-off factor
-# p: time samples of the filter
-# num_samples: number of samples, must be a even number
-# T_samples: number of samples per signal period
-    
-    Ts = 1
-    t = (1/Ts)*np.linspace(-Ts/np.sqrt(1+beta/2), Ts/np.sqrt(1+beta/2), N)
-    p = np.zeros(len(t));
-    for i in np.arange(len(t)):
-        p[i] = (4*beta/(np.pi*np.sqrt(Ts)))*(np.cos((1+beta)*np.pi*t[i]/Ts) +np.sin((1-beta)*np.pi*t[i]/Ts)/(4*beta*t[i]/Ts))/(1-(4*beta*t[i]/Ts)**2)
-        
-    t_0 = 0
-    t_0 = [i for i in range(len(t)) if t[i] == 0.0] 
-    if (t_0 != 0):
-        p[t_0] = (4*beta/(np.pi*np.sqrt(Ts)))*(1 + (1-beta)*np.pi/(4*beta))
 
-    t_1 = Ts/(4*beta)
-    t_1p = 0
-    t_1m = 0
-    t_1p =  [i for i in range(len(t)) if t[i] == t_1]
-    t_1m = [i for i in range(len(t)) if t[i] == -t_1]
-    if (t_1p != 0):
-        p[t_1p] = (beta/np.sqrt(2*Ts))*((1+2/np.pi)*np.sin(np.pi/(4*beta)) + (1-2/np.pi)*np.cos(np.pi/(4*beta)))
-        p[t_1m] = (beta/np.sqrt(2*Ts))*((1+2/np.pi)*np.sin(np.pi/(4*beta)) + (1-2/np.pi)*np.cos(np.pi/(4*beta)))
-      
-    p = p - np.amin(p)
-    # Guarantizes normalized energy
-    p = p/np.sum(p)
-
-    return np.resize(p, (100,1))
 
 #gardner_TED(data_conv)
-rrc_filter(N, data_conv.real)
+#rrc_filter(N, data_conv.real)
+
+
+
+
+
 
 
